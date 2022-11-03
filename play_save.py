9148791c6da4cb5d1692cc9769fe6ec1deeab73a
DarkBlueCheese <dolazy7@gmail.com>
2022-11-02 19:03:02 -0500

    Nov 02 1902 finished periodic boundary condition

--- play_save.py
+++ play_save.py
@@ -17,9 +17,68 @@ color_alive_next = (255, 255, 255)
 
 def update(screen, cells, size, with_progress=False):
     updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
-
+    row_m, row_max, col_m, col_max = 0, (len(cells) - 1), 0, (len(cells[0]) - 1)
     for row, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        # print(mat_mat2[row, col])
+
+        # color = color_bg if cells[row, col] == 0 else color_alive_next
+        
+        # xmin ymin (0, 0)
+        
+        if (row == row_m) and (col == col_m):
+            # print(f'xmin, ymin : {cells[row, col]}')
+            # xmin_ymin_list.append((row, col))
+            alive = cells[row_m, col_m+1] + cells[row_m+1, col_m] + cells[row_m+1, col_m+1] + cells[row_max, col_m] + \
+                cells[row_max, col_m+1] + cells[row_m, col_max] + cells[row_m+1, col_max] + cells[row_max, col_max]
+
+        # xmax, ymax (2, 2)
+        elif (row == row_max) and (col == col_max):
+            # print(f'xmax, ymax : {cells[row, col]}')
+            # xmax_ymax_list.append((row, col))
+            alive = cells[row_max, col_max-1] + cells[row_max-1, col_max] + cells[row_max-1, col_max-1] + cells[row_m, col_max] + \
+                cells[row_m, col_max-1] + cells[row_max, col_m] + cells[row_max-1, col_m] + cells[row_m, col_m]
+        # xmin ymax (0, 2)
+        elif (row == row_m) and (col == col_max):
+            # print(f'xmin, ymax : {cells[row, col]}')
+            # xmin_ymax_list.append((row, col))
+            alive = cells[row_m, col_max-1] + cells[row_m+1, col_max] + cells[row_m+1, col_max-1] + cells[row_max, col_max] + \
+                cells[row_max, col_max-1] + cells[row_m, col_m] + cells[row_m+1, col_m] + cells[row_max, col_m]
+        # xmax ymin (2, 0)
+        elif (row == row_max) and (col == col_m):
+            # print(f'xmax, ymin : {cells[row, col]}')
+            # xmax_ymin_list.append((row, col))
+            alive = cells[row_max, col_m+1] + cells[row_max-1, col_m] + cells[row_max-1, col_m+1] + cells[row_m, col_m] + \
+                cells[row_m, col_m+1] + cells[row_max, col_max] + cells[row_max-1, col_max] + cells[row_m, col_max]
+        # xmin
+        elif (row == row_m):
+        # print(f'xmin : {cells[row, col]}')
+        # xmin_list.append((row, col))
+            alive = np.sum(cells[row:row+2, col-1:col+2]) - cells[row, col] + np.sum(cells[row-1, col-1:col+2])
+        # xmax
+        elif (row == row_max):
+            # print(f'xmax : {cells[row, col]}')
+            # xmax_list.append((row, col))
+            alive = np.sum(cells[row-1:row+1, col-1:col+2]) - cells[row, col] + np.sum(cells[row-(len(cells)-1), col-1:col+2])
+        # ymin
+        elif (col == col_m):
+            # print(f'ymin : {cells[row, col]}')
+            # ymin_list.append((row, col))
+            alive = np.sum(cells[row-1:row+2, col:col+2]) - cells[row, col] + np.sum(cells[row-1:row+2, col-1])
+        # ymax
+        elif (col == col_max):
+            # print(f'ymin : {cells[row, col]}')
+            # ymax_list.append((row, col))
+            alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col] + np.sum(cells[row-1:row+2, col-(len(cells[0])-1)])
+        else:
+            # print(f'else : {cells[row, col]}')
+            # the_rest_list.append((row, col))
+            alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+
+
+
+
+
+
         color = color_bg if cells[row, col] == 0 else color_alive_next
         
         if cells[row, col] == 1:
